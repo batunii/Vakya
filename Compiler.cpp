9df1aec2e48c68ab5fe9d47e1d1b97483758849c
batunii <sy.soni@live.com.sg>
2025-07-04 21:14:22 +0530

    This patch fixes the next token function and changes types

--- Compiler.cpp
+++ Compiler.cpp
@@ -6,41 +6,36 @@
 #include <string>
 #include <vector>
 
-/*
-parse the tokens => 
-if you get Do =>
-call parse_do function
-parse do function will create a new Program object
-assign it to curr_program var
-and make the do_token inside the program var
-do same with on but don't create a new program var for it. 
-*/
-
 class AST {
-  int curr_token = -1;
-  int next_token = 0;
+  size_t curr_token = 0;
+  size_t next_token = 1;
   Lexer *lexer;
-	std::vector<Program> program_steps;
+  std::vector<Program> program_steps;
   Program *curr_program;
+
 public:
   std::optional<Tokens> advance_token() {
     if (curr_token < lexer->t_list.size()) {
       ++next_token;
-      return lexer->t_list[++curr_token];
+      return lexer->t_list[this->curr_token++];
     } else
       return std::nullopt;
   }
-  Tokens peek_token() { return lexer->t_list[next_token]; }
-};
+  std::optional<Tokens> peek_token() {
+    std::optional<Tokens> next_token = this->advance_token();
+    --this->curr_token;
+    --this->next_token;
+    return next_token;
+  };
 
-int main() {
-  std::string code, line;
-  std::cout << "Vakya (enter multiple lines, Ctrl+D to end input):\n";
-  while (std::getline(std::cin, line)) {
-    code += line + "\n"; // Preserve line breaks
+  int main() {
+    std::string code, line;
+    std::cout << "Vakya (enter multiple lines, Ctrl+D to end input):\n";
+    while (std::getline(std::cin, line)) {
+      code += line + "\n"; // Preserve line breaks
+    }
+
+    Lexer lexer(code);
+    std::cout << lexer.make_tokens() << std::endl;
+    return 0;
   }
-
-  Lexer lexer(code);
-  std::cout << lexer.make_tokens() << std::endl;
-  return 0;
-}
